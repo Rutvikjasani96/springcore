ff732160236e827a90fcde783518b296248a8549
Rutvik Jasani <rutvikjasani96@gmail.com>
2024-05-29 16:37:21 +0530

    Video 6 Work done.

---
+++ src/main/java/com/springcore/Student.java
@@ -0,0 +1,55 @@
+package com.springcore;
+
+public class Student {
+    private int studentId;
+    private String studentName;
+    private String studentAddress;
+
+    //getters
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+
+    //setters
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+
+    //constructor
+    public Student(int studentId, String studentName, String studentAddress) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.studentAddress = studentAddress;
+    }
+
+    //constructor using super class
+    public Student() {
+    }
+
+    // toString Method
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", studentName='" + studentName + '\'' +
+                ", studentAddress='" + studentAddress + '\'' +
+                '}';
+    }
+}
