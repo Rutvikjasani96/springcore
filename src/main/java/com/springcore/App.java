34c1d3bc47af53024ec1687ea659e5ed0afc0abf
Rutvik Jasani <rutvikjasani96@gmail.com>
2024-05-29 18:19:44 +0530

    Another Way of creating bean

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -14,7 +14,11 @@ public class App
     {
         System.out.println( "Hello World!" );
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
         Student student1 = (Student) context.getBean("student1");
+        Student student2 = (Student) context.getBean("student2");
+
         System.out.println(student1);
+        System.out.println(student2);
     }
 }
